1486dd4217745042613777111aa2b6e069e21dc1
kakyoindonut321 <forbiddenikki@gmail.com>
2022-10-11 09:51:13 +0700

    upload project ke github, masih tahap percobaan blm pengerjaan

---
+++ database/migrations/2022_10_11_011410_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id("product_id");
+            $table->string('name');
+            $table->decimal('price', $precision = 12, $scale = 2);
+            $table->string('description');
+            $table->integer('stock');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
